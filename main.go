fd718d47df737324aa8deea23d16d4392e269f16
Skip Tavakkolian <skip.tavakkolian@gmail.com>
2016-12-22 10:06:51 -0800

    added Azure user info

--- main.go
+++ main.go
@@ -4,7 +4,20 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"html/template"
 )
+
+const homePage = `
+<html>
+  <head>
+    <title>Authentic with pizzazz!</title>
+  </head>
+  <body>
+    <h1>Hello {{.}}!</h1>
+    <p><i>You are authentic.</i></p>
+  </body>
+</html>
+`
 
 func main() {
 	port := os.Getenv("HTTP_PLATFORM_PORT")
@@ -19,6 +32,16 @@ func main() {
 	}
 
 	addr := ":"+port
-	log.Fatal(http.ListenAndServe(addr, http.FileServer(http.Dir(root))))
+
+	tmpl := template.Must(template.New("homepage").Parse(homePage))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		name, ok := r.Header["X-MS-CLIENT-PRINCIPAL-NAME"]
+		if !ok {
+			name = []string{"Unknown"}
+		}
+		tmpl.Execute(w, name[0])
+	})
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
